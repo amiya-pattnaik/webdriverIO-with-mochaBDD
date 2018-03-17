188981dacb5b3fbf62b32edb5f5f0f346e408bb4
pattnaik_amiya <pattnaik_amiya@yahoo.com>
2018-03-16 23:48:21 -0500

    initial commit

---
+++ Gruntfile.js
@@ -0,0 +1,19 @@
+module.exports = function(grunt) {
+
+    grunt.initConfig({
+        pkg: grunt.file.readJSON('package.json'),
+        webdriver: {
+            test: {
+                configFile: './test/config/suite.mocha.conf.js'
+            },
+            test-mobile: {
+                configFile: './test/config/suite.appium.conf.js'
+            },
+        },
+    });
+
+    grunt.loadNpmTasks('grunt-mocha');
+    grunt.loadNpmTasks('grunt-webdriver');
+    grunt.registerTask('default', ['webdriver:test']);
+    //grunt.registerTask('default', ['webdriver:test-mobile']);
+};
